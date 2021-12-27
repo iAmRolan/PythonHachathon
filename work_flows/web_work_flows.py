dabbe4486fb8ec6a5f53e7d29def998fd7bf009a
Rolan <rolan.liev@gmail.com>
2021-12-27 14:51:18 +0200

    Allure annotations added.

--- work_flows/web_work_flows.py
+++ work_flows/web_work_flows.py
@@ -1,3 +1,5 @@
+import allure
+
 import utilities
 from extensions.UIActions import UIActions
 from utilities.common_ops import CommonOps
@@ -7,6 +9,7 @@ from utilities.manage_pages import ManagePages
 class WebWorkFlows:
 
     @staticmethod
+    @allure.step("Login to RWA")
     def login():
         UIActions.send_keys_to_element(utilities.manage_pages.login_page.get_username_input(),
                                        CommonOps.get_data("UserName"))
@@ -15,6 +18,7 @@ class WebWorkFlows:
         UIActions.click_element(utilities.manage_pages.login_page.get_login_button())
 
     @staticmethod
+    @allure.step("Login new user")
     def login_new_user():
         UIActions.send_keys_to_element(utilities.manage_pages.login_page.get_username_input(),
                                        CommonOps.get_data("NewUserName"))
@@ -23,6 +27,7 @@ class WebWorkFlows:
         UIActions.click_element(utilities.manage_pages.login_page.get_login_button())
 
     @staticmethod
+    @allure.step("Sign up new user")
     def sign_up():
         UIActions.click_element(utilities.manage_pages.login_page.get_signup_button())
         UIActions.click_element(utilities.manage_pages.login_page.get_signup_button())
@@ -39,6 +44,7 @@ class WebWorkFlows:
         UIActions.click_element(utilities.manage_pages.signup_form.get_signup_button())
 
     @staticmethod
+    @allure.step("First login after sign up")
     def after_first_sign_up():
         UIActions.click_element(utilities.manage_pages.home_page.get_signup_next_button())
         UIActions.send_keys_to_element(utilities.manage_pages.home_page.get_signup_bank_acc_input(),
@@ -51,10 +57,12 @@ class WebWorkFlows:
         UIActions.click_element(utilities.manage_pages.home_page.get_done_button())
 
     @staticmethod
+    @allure.step("Logout RWA")
     def logout():
         UIActions.click_element(utilities.manage_pages.home_page.get_logout_button())
 
     @staticmethod
+    @allure.step("Money transactions")
     def make_transaction():
         UIActions.click_element(utilities.manage_pages.home_page.get_transaction_button())
         UIActions.click_element(utilities.manage_pages.transactions_page.get_users()[1])
@@ -67,6 +75,7 @@ class WebWorkFlows:
         UIActions.click_element(utilities.manage_pages.transactions_page.get_transactions_list()[0])
 
     @staticmethod
+    @allure.step("Bank account cration")
     def create_bank_account():
         UIActions.click_element(utilities.manage_pages.home_page.get_bank_accounts_button())
         UIActions.click_element(utilities.manage_pages.bank_account_page.get_create_button())
@@ -79,9 +88,11 @@ class WebWorkFlows:
         UIActions.click_element(utilities.manage_pages.bank_account_page.get_save_button())
 
     @staticmethod
+    @allure.step("Navigate to notfications")
     def navigate_to_notifications():
         UIActions.click_element(utilities.manage_pages.home_page.get_notification_button())
 
     @staticmethod
+    @allure.step("Delete notification")
     def delete_notification():
         UIActions.click_element(utilities.manage_pages.notifications_page.get_notification_dismiss_button())
